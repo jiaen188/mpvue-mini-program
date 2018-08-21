3a8b6959ff625806b2a00f40c3137328fd407a25
jiaen188 <244189981@qq.com>
2018-08-21 17:24:50 +0800

    本地服务的配置

--- server/config.js
+++ server/config.js
@@ -1,4 +1,14 @@
 const CONF = {
+    serverHost: 'localhost',
+    tunnelServerUrl: '',
+    tunnelSignatureKey: '27fb7d1c161b7ca52d73cce0f1d833f9f5b5ec89',
+    // 腾讯云相关配置可以查看云 API 秘钥控制台：https://console.cloud.tencent.com/capi
+    qcloudAppId: '1257076991',
+    qcloudSecretId: 'AKID76tTq0uhEZhcLrfHBrLdiZDesujLlwvi',
+    qcloudSecretKey: 'hBSYR8duyqVwAB0ZtxlJbdPwDEyTihQt',
+    wxMessageToken: 'weixinmsgtoken',
+    networkTimeout: 30000,
+
     port: '5757',
     rootPathname: '',
 
@@ -21,7 +31,7 @@ const CONF = {
         port: 3306,
         user: 'root',
         db: 'cAuth',
-        pass: 'wx8613c6d830b0f694',
+        pass: '',
         char: 'utf8mb4'
     },
 
