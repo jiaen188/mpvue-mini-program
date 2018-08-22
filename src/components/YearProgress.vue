aa1cc8d492650849ca6de474cc0b759340c9bd13
jiaen188 <244189981@qq.com>
2018-08-22 19:44:16 +0800

    今年过了多少天组件

--- src/components/YearProgress.vue
+++ src/components/YearProgress.vue
@@ -1,13 +1,56 @@
 <template>
-  <div class="progress-bar">进度条</div>
+  <div class="progress-bar">
+    <progress :percent="percent" activeColor="#EA5A49"></progress>
+    <p>{{year}}已经过去了{{days}}天，{{percent}}%</p>
+  </div>
 </template>
 
 <script>
 export default {
+  methods: {
+    isLeapYeader() {
+      const year = new Date().getFullYear()
+      if (year % 400 === 0) {
+        return true
+      } else if (year % 4 == 0 && year % 100 !== 0) {
+        return true
+      } else {
+        return false
+      }
+    },
+    getDayOfYear() {
+      return this.isLeapYeader() ? 366 : 365
+    }
+  },
+  computed: {
+    year () {
+      return new Date().getFullYear()
+    },
+    days () {
+      let start = new Date()
+      // start就是今年第一天
+      start.setMonth(0)
+      start.setDate(1)
 
+      // 今天的时间戳 减去今年第一天的时间戳
+      let offset = new Date().getTime() - start.getTime()
+      return parseInt(offset / 1000 / 60 / 60 / 24) + 1
+    },
+    percent () {
+      return (this.days * 100 / this.getDayOfYear()).toFixed(1)
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
-
+.progress-bar {
+  text-align: center;
+  margin-top: 10px;
+  margin-bottom: 40px;
+  width: 100%;
+  progress {
+    margin-bottom: 10px;
+  }
+}
 </style>
