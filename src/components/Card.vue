622f7185537e93e7a657cfe867a58d04229a388d
jiaen188 <244189981@qq.com>
2018-08-23 20:51:00 +0800

    图书卡片显示数据

---
+++ src/components/Card.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="book-card">
+    <div class="thumb">
+      <img :src="book.image" mode="aspectFit" class="image">
+    </div>
+    <div class="detail">
+      <div class="row">
+        <div class="right">
+          {{book.rate}}
+        </div>
+        <div class="left">
+          {{book.title}}
+        </div>
+      </div>
+      <div class="row">
+        <div class="right">
+          浏览量：
+        </div>
+        <div class="left">
+          {{book.author}}
+        </div>
+      </div>
+      <div class="row">
+        <div class="right">
+          添加人
+        </div>
+        <div class="left">
+          {{book.publisher}}
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['book']
+}
+</script>
+
+<style lang="scss" scoped>
+.book-card {
+  padding: 5px;
+  overflow: hidden;
+  margin-top: 5px;
+  margin-bottom: 5px;
+  font-size: 14px;
+  .thumb {
+    width: 90px;
+    height: 90px;
+    float: left;
+    margin: 0 auto;
+    overflow: hidden;
+    .image {
+      max-width: 100%;
+      max-height: 100%;
+    }
+  }
+  .detail {
+    margin-left: 100px;
+    .row {
+      line-height: 20px;
+      margin-bottom: 3px;
+      .right {
+        float: right;
+      }
+      .left {
+        margin-right: 80px;
+      }
+    }
+  }
+}
+</style>
--- src/pages/books/Books.vue
+++ src/pages/books/Books.vue
@@ -1,11 +1,12 @@
 <template>
   <div>
-    <div v-for="book in books" :key="book.id">{{book.title}}</div>
+    <card v-for="book in books" :key="book.id" :book="book"></card>
   </div>
 </template>
 
 <script>
 import { get } from '../../util.js'
+import Card from '../../components/Card'
 
 export default {
   data () {
@@ -21,6 +22,9 @@ export default {
       const books = await get('/weapp/booklist')
       this.books = books.list
     }
+  },
+  components: {
+    Card
   }
 }
 </script>
