50e3e20c002ac0d4b9a977f78623f5fe7e691795
jiaen188 <244189981@qq.com>
2018-08-29 15:41:49 +0800

    图书列表后端修改

--- server/controllers/booklist.js
+++ server/controllers/booklist.js
@@ -1,15 +1,21 @@
 const { mysql } = require('../qcloud')
 
 module.exports = async (ctx) => {
-    const { page } = ctx.request.query
+    const { page, openid } = ctx.request.query
     const size = 10
-    const books = await mysql('books')
+    const mysqlSelect = mysql('books')
                     .select('books.*', 'cSessionInfo.user_info')
                     // 链表查询，查询cSessionInf的user_info，对应books的openid和cSessionInf的open_id相等的数据
                     .join('cSessionInfo', 'books.openid', 'cSessionInfo.open_id')
-                    .limit(size)
-                    .offset(Number(page) * size)
                     .orderBy('books.id', 'desc')
+    let books
+    if (openid) {
+        // 根据openid过滤
+        books = await mysqlSelect.where('books.openid', openid)
+    } else {
+        // 全部图书，分页
+        books = await mysqlSelect.limit(size).offset(Number(page) * size)
+    }
     ctx.state.data = {
         list: books.map(v => {
             const info = JSON.parse(v.user_info)
