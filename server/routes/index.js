0fbd65090af102a092ae2ee8e96760f174e9f9d7
jiaen188 <244189981@qq.com>
2018-09-11 17:45:17 +0800

    后台代码注释 无用代码

--- server/routes/index.js
+++ server/routes/index.js
@@ -32,12 +32,12 @@ router.get('/message', controllers.message.get)
 // POST 用来处理微信转发过来的客服消息
 router.post('/message', controllers.message.post)
 
-router.get('/demo', controllers.demo)
+// router.get('/demo', controllers.demo)
+
 router.post('/addbook', controllers.addbook)
 router.get('/booklist', controllers.booklist)
 router.get('/bookdetail', controllers.bookdetail)
 router.get('/top', controllers.top)
-
 router.post('/addcomment', controllers.addcomment)
 router.get('/commentlist', controllers.commentlist)
 
